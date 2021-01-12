dc42a2095d79b39b91ee258001059d9f216248a1
정 선 미 <kmsun0720@naver.com>
2021-01-13 01:54:24 +0900

    Add: 로그인 기능 구현 추가

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,33 +7,61 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      condition: false,
+      emailCondition: true,
+      passwordCondition: true,
+      matchedValue: true,
     };
   }
 
   handleIdPwInput = (e) => {
     const { name, value } = e.target;
-    this.setState(
-      {
-        [name]: value,
-      },
-      () => this.checkBtnActive(),
-    );
-  };
-
-  checkBtnActive = () => {
-    const { email, password } = this.state;
-    //const emailCondition=email.includes("@")       // condition 작동 시 지우기
-    const condition = email.includes("@") && password;
     this.setState({
-      condition,
+      [name]: value,
     });
   };
 
-  // 버튼 클릭시 메소드 수정
-  handleLoginBtn = (e) => {};
+  handleLoginBtn = (e) => {
+    const { email, password } = this.state;
+    const emailCondition = email.includes("@");
+    const passwordCondition = password.length >= 5;
+    this.setState({
+      emailCondition,
+      passwordCondition,
+    });
+
+    if (!emailCondition || !passwordCondition) {
+      console.log("!emailCondition || !passwordCondition 실행");
+      return; // 리턴 하는 게 맞나?
+    }
+
+    fetch("api주소", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((responce) => responce.json())
+      .then((result) => {
+        console.log({ result }); // 백의 결과 값을 보고 키명과 메세지 수정 가능
+
+        if (result.message === "SUCCESS") {
+          // 메세지 키명: 콘솔 확인 후 백과 상의
+          localStorage.setItem("token", token.Authorization); // 토큰 키명: 콘솔 확인 후 백과 상의
+          this.props.history.push("/");
+          return;
+        }
+
+        // result.message === "SUCCESS"가 아닐 경우
+        this.setState({
+          matchedValue: false,
+        });
+        return;
+      });
+  };
 
   render() {
+    const { emailCondition, passwordCondition } = this.state;
     return (
       <div className="login">
         <main>
@@ -43,6 +71,7 @@ class Login extends Component {
           </section>
           <section className="inputBundle">
             <div className="emailBundle">
+              {matchedValue ? "" : <p>* 잘못된 로그인 시도</p>}
               <p>이메일</p>
               <input
                 className="email loginInput"
@@ -50,6 +79,7 @@ class Login extends Component {
                 name="email"
                 onChange={this.handleIdPwInput}
               />
+              {emailCondition ? "" : <div>이메일은 필수입니다.</div>}
             </div>
             <div className="pwBundle">
               <p>비밀번호</p>
@@ -59,6 +89,7 @@ class Login extends Component {
                 name="password"
                 onChange={this.handleInput}
               />
+              {passwordCondition ? "" : <div>비밀번호는 필수 입니다.</div>}
             </div>
             <input type="checkbox" name="checkbox" />
             <span>사용자 이름 저장</span>
@@ -77,7 +108,14 @@ class Login extends Component {
           <button className="googleBtn"></button>
           <button className="naverBtn"></button>
         </aside>
-        <footer></footer>
+        <footer>
+          <span>KOREAN</span>
+          <div>
+            <a>@ 2021 Bang & Olaf</a>
+            <a>개인정보 보호정책</a>
+            <a>서비스 약관</a>
+          </div>
+        </footer>
       </div>
     );
   }
