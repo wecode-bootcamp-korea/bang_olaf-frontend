ca8d32fb0725268f8264260d7efbe32542642594
정 선 미 <kmsun0720@naver.com>
2021-01-14 02:46:02 +0900

    Modify: 레이아웃 스타일(scss) 수정

--- src/Pages/Account/Account.js
+++ src/Pages/Account/Account.js
@@ -9,78 +9,120 @@ class Account extends React.Component {
       lastname: "",
       email: "",
       password: "",
+      //pwLengthCondition: true,
       checkPw: "",
+      //correction: true,
       adagreement: false,
       serviceagreement: false,
+      possibleRegisterConditon: false,
     };
   }
 
   handleInput = (e) => {
     const { name, value, checked } = e.target;
-    this.setState({
-      [name]: value,
-      [name]: checked,
-    });
+    this.setState(
+      {
+        [name]: value,
+        [name]: checked,
+      },
+      // () => this.registerCondition(),
+      // () => this.pwLength(),
+    );
   };
 
-  registerCondition = () => {
-    // 로그인 버튼 클릭시 사용할 조건
-    const { email, password } = this.state;
-
-    const emailCondition = email.includes("@");
+  // 비밀번호 길이 조건식 + 비밀번호 일치 조건식
+  /*pwLength = () => {
+    const { password} = this.state;
     const pwLengthCondition = password.length >= 10 && password.length <= 30;
-    const 
-  };
+    //const correction = password === checkPw;
+    this.setState({
+      pwLengthCondition,
+      //correction,
+    });
+  };*/
+
+  /*registerCondition = () => {
+    // 유효성 검사
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      // pwLengthCondition,
+      checkPw,
+      serviceagreement,
+    } = this.state; // 다 넣었는지 추후 확인
+
+    //const emailCondition = email.includes("@");
+    const possibleRegisterConditon =
+      firstname &&
+      lastname &&
+      //emailCondition &&
+      password &&
+      checkPw &&
+      serviceagreement;
+
+    this.setState({
+      possibleRegisterConditon: possibleRegisterConditon,
+    });
+  };*/
+
+  /*handleBtn = () => {
+    
+  };*/
 
   render() {
     console.log(this.state);
     return (
       <div className="account">
+        <nav>네브바 자리</nav>
         <main>
           <section className="accountTitle">새 계정 만들기</section>
           <section className="inputCheckboxBundle">
             <article className="inputBundle">
-              <div className="firstname accountInput">
+              <div className="firstname accountTextInput">
                 <p>이름</p>
                 <input
                   type="text"
                   name="firstname"
                   onChange={this.handleInput}
                 />
-                <p>이름은 필수 입력 항목입니다.</p>
+                <p className="warningMsg">이름은 필수 입력 항목입니다.</p>
               </div>
-              <div className="lastname accountInput">
+              <div className="lastname accountTextInput">
                 <p>성</p>
                 <input
                   type="text"
                   name="lastname"
                   onChange={this.handleInput}
                 />
-                <p>성은 필수 입력 항목입니다.</p>
+                <p className="warningMsg">성은 필수 입력 항목입니다.</p>
               </div>
-              <div className="email accountInput">
+              <div className="email accountTextInput">
                 <p>이메일</p>
                 <input type="text" name="email" onChange={this.handleInput} />
-                <p>이메일은 필수 입력 항목입니다.</p>
+                <p className="warningMsg">이메일은 필수 입력 항목입니다.</p>
               </div>
-              <div className="password accountInput">
+              <div className="password accountTextInput">
                 <p>비밀번호</p>
                 <input
                   type="password"
                   name="password"
                   onChange={this.handleInput}
                 />
-                <p>비밀번호은 필수 입력 항목입니다.</p>
-                <p>비밀번호는 최소 10자, 최대 30자여야 합니다.</p>
+                <p className="warningMsg">비밀번호은 필수 입력 항목입니다.</p>
+                <p className="warningMsg">
+                  비밀번호는 최소 10자, 최대 30자여야 합니다.
+                </p>
               </div>
-              <div className="checkPw accountInput">
+              <div className="checkPw accountTextInput">
                 <p>비밀번호 확인</p>
                 <input
                   type="password"
                   name="checkPw"
                   onChange={this.handleInput}
                 />
-                <p>비밀번호가 일치 하지 않습니다.</p>
+                <p className="warningMsg">비밀번호가 일치 하지 않습니다.</p>
               </div>
             </article>
             <article className="checkboxBundle">
@@ -90,8 +132,10 @@ class Account extends React.Component {
                   name="adagreement"
                   onClick={this.handleInput}
                 />
-                <span>네, Bang&Olaf 통신을 받고 싶습니다</span>
-                <div className="communicationDescription">
+                <span className="agreement adtitle">
+                  네, Bang&Olaf 통신을 받고 싶습니다
+                </span>
+                <div className="agreement adDescription">
                   Bang & Olufsen 및 당사 제품과 관련된 뉴스, 특별 혜택, 이벤트
                   및 특별 경연 초대장 등의 소식을 가장 먼저 받아보세요. 저희가
                   여러분께 소식을 가장 먼저 전해드리기 위해 여러분의 연락처
@@ -104,12 +148,14 @@ class Account extends React.Component {
                   name="serviceagreement"
                   onClick={this.handleInput}
                 />
-                <span>동의 서비스 약관 & 개인정보 보호정책</span>
+                <span className="agreement service">
+                  동의 서비스 약관 & 개인정보 보호정책
+                </span>
               </div>
             </article>
           </section>
           <section className="registerMovetologinBundle">
-            <button onClick={this.handleBtn}>등록</button>
+            <button>등록</button>
             <div>페이지의 로그로 이동</div>
           </section>
         </main>
