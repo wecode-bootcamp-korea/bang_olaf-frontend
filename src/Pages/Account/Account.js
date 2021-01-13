3d0f124abc58e0a1ac333d8b866aa74a3112a860
정 선 미 <kmsun0720@naver.com>
2021-01-13 21:22:07 +0900

    Add: Account 레이아웃 및 기능 구현

---
+++ src/Pages/Account/Account.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "./Account.scss";
+
+class Account extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      checkPw: "",
+      adagreement: false,
+      serviceagreement: false,
+    };
+  }
+
+  handleInput = (e) => {
+    const { name, value, checked } = e.target;
+    this.setState({
+      [name]: value,
+      [name]: checked,
+    });
+  };
+
+  registerCondition = () => {
+    // 로그인 버튼 클릭시 사용할 조건
+    const { email, password } = this.state;
+
+    const emailCondition = email.includes("@");
+    const pwLengthCondition = password.length >= 10 && password.length <= 30;
+    const 
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="account">
+        <main>
+          <section className="accountTitle">새 계정 만들기</section>
+          <section className="inputCheckboxBundle">
+            <article className="inputBundle">
+              <div className="firstname accountInput">
+                <p>이름</p>
+                <input
+                  type="text"
+                  name="firstname"
+                  onChange={this.handleInput}
+                />
+                <p>이름은 필수 입력 항목입니다.</p>
+              </div>
+              <div className="lastname accountInput">
+                <p>성</p>
+                <input
+                  type="text"
+                  name="lastname"
+                  onChange={this.handleInput}
+                />
+                <p>성은 필수 입력 항목입니다.</p>
+              </div>
+              <div className="email accountInput">
+                <p>이메일</p>
+                <input type="text" name="email" onChange={this.handleInput} />
+                <p>이메일은 필수 입력 항목입니다.</p>
+              </div>
+              <div className="password accountInput">
+                <p>비밀번호</p>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={this.handleInput}
+                />
+                <p>비밀번호은 필수 입력 항목입니다.</p>
+                <p>비밀번호는 최소 10자, 최대 30자여야 합니다.</p>
+              </div>
+              <div className="checkPw accountInput">
+                <p>비밀번호 확인</p>
+                <input
+                  type="password"
+                  name="checkPw"
+                  onChange={this.handleInput}
+                />
+                <p>비밀번호가 일치 하지 않습니다.</p>
+              </div>
+            </article>
+            <article className="checkboxBundle">
+              <div className="firstCheckboxBundle">
+                <input
+                  type="checkbox"
+                  name="adagreement"
+                  onClick={this.handleInput}
+                />
+                <span>네, Bang&Olaf 통신을 받고 싶습니다</span>
+                <div className="communicationDescription">
+                  Bang & Olufsen 및 당사 제품과 관련된 뉴스, 특별 혜택, 이벤트
+                  및 특별 경연 초대장 등의 소식을 가장 먼저 받아보세요. 저희가
+                  여러분께 소식을 가장 먼저 전해드리기 위해 여러분의 연락처
+                  정보를 수집해야 합니다. 당사 통신에 대한 자세한 내용 읽기
+                </div>
+              </div>
+              <div className="secondCheckboxBundle">
+                <input
+                  type="checkbox"
+                  name="serviceagreement"
+                  onClick={this.handleInput}
+                />
+                <span>동의 서비스 약관 & 개인정보 보호정책</span>
+              </div>
+            </article>
+          </section>
+          <section className="registerMovetologinBundle">
+            <button onClick={this.handleBtn}>등록</button>
+            <div>페이지의 로그로 이동</div>
+          </section>
+        </main>
+        <footer>
+          <span>KOREAN</span>
+          <div>
+            <a>@ 2021 Bang & Olaf</a>
+            <a>개인정보 보호정책</a>
+            <a>서비스 약관</a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Account;
