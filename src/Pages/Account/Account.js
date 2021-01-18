5e4523c7bddcf6c97b5a7fe51b97ad37479daeea
정 선 미 <kmsun0720@naver.com>
2021-01-18 19:32:51 +0900

    Refactor: 스스로 리팩토링

--- src/Pages/Account/Account.js
+++ src/Pages/Account/Account.js
@@ -8,39 +8,67 @@ class Account extends React.Component {
     super();
     this.state = {
       firstname: "",
+      firstnamehasValue: true,
       lastname: "",
       email: "",
       password: "",
+      passwordhasValue: true,
       checkPw: "",
+      checkPwhasValue: true,
       adagreement: false,
       serviceagreement: false,
+      serviceagreementhasValue: true,
     };
   }
 
   // 인풋의 변화연동
   firstnameInput = (e) => {
     const firstnameValue = e.target.value;
-    this.setState({ firstname: firstnameValue });
+    this.setState(
+      {
+        firstname: firstnameValue,
+      },
+      () =>
+        this.setState({
+          firstnamehasValue: this.state.firstname.length > 0 ? true : false,
+        }),
+    );
   };
 
   lastnameInput = (e) => {
     const lastnameValue = e.target.value;
-    this.setState({ lastname: lastnameValue });
+    this.setState({ lastname: lastnameValue }, () =>
+      this.setState({
+        lastnamehasValue: this.state.lastname.length > 0 ? true : false,
+      }),
+    );
   };
 
   emailInput = (e) => {
     const emailValue = e.target.value;
-    this.setState({ email: emailValue });
+    this.setState({ email: emailValue }, () =>
+      this.setState({
+        emailhasValue: this.state.email.length > 0 ? true : false,
+      }),
+    );
   };
 
   passwordInput = (e) => {
     const passwordValue = e.target.value;
-    this.setState({ password: passwordValue });
+    this.setState({ password: passwordValue }, () =>
+      this.setState({
+        passwordhasValue: this.state.password.length > 0 ? true : false,
+      }),
+    );
   };
 
   checkPwInput = (e) => {
     const checkPwValue = e.target.value;
-    this.setState({ checkPw: checkPwValue });
+    this.setState({ checkPw: checkPwValue }, () =>
+      this.setState({
+        checkPwhasValue: this.state.checkPw.length > 0 ? true : false,
+      }),
+    );
   };
 
   adagreementInput = (e) => {
@@ -50,104 +78,17 @@ class Account extends React.Component {
 
   seviceagreementInput = (e) => {
     const serviceagreementValue = e.target.checked;
-    this.setState({ seviceagreement: serviceagreementValue });
+    this.setState({ seviceagreement: serviceagreementValue }, () =>
+      this.setState({
+        serviceagreementhasValue: this.state.seviceagreement ? true : false,
+      }),
+    );
   };
 
-  // 체크박스의 변화연동
-  // handleCheckbox = (e) => {
-  //   const { name, checked } = e.target;
-  //   this.setState(
-  //     {
-  //       [name]: checked,
-  //     },
-  //     () => this.secondCondition(),
-  //   );
-  // };
-
-  // onChange msg
-  secondCondition = (e) => {
-    // const {
-    //   firstname,
-    //   firstnamehasValue,
-    //   lastname,
-    //   lastnamehasValue,
-    //   email,
-    //   emailCondition,
-    //   emailhasValue,
-    //   password,
-    //   pwLengthCondition,
-    //   passwordhasValue,
-    //   checkPw,
-    //   checkpwCondition,
-    //   checkPwhasValue,
-    //   adagreement,
-    //   serviceagreement,
-    //   isTrueserviceagreement,
-    // } = this.state;
     // const checkemailCondition = email.includes("@"); // 이메일 @ 확인
     // const checkpwLengthCondition =
     //   password.length >= 10 && password.length <= 30; // 비번 길이
     // const checkpwConditionAll = password === checkPw; // 비번확인 모든 조건
-    // if (firstname) {
-    //   this.setState({
-    //     firstnamehasValue: true,
-    //   });
-    // } else if (!firstname) {
-    //   this.setState({
-    //     firstnamehasValue: false,
-    //   });
-    // }
-    // if (lastname) {
-    //   this.setState({
-    //     lastnamehasValue: true,
-    //   });
-    // } else if (!lastname) {
-    //   this.setState({
-    //     lastnamehasValue: false,
-    //   });
-    // }
-    // if (email) {
-    //   this.setState({
-    //     emailCondition: checkemailCondition,
-    //     emailhasValue: true,
-    //   });
-    // } else if (!email) {
-    //   this.setState({
-    //     emailCondition: true,
-    //     emailhasValue: false,
-    //   });
-    // }
-    // if (checkPw && password) {
-    //   this.setState({
-    //     checkpwCondition: checkpwConditionAll,
-    //     checkPwhasValue: true,
-    //   });
-    // } else if (!checkPw && password) {
-    //   this.setState({
-    //     checkpwCondition: true,
-    //     checkPwhasValue: false,
-    //   });
-    // }
-    // if (password) {
-    //   this.setState({
-    //     pwLengthCondition: checkpwLengthCondition,
-    //     passwordhasValue: true,
-    //   });
-    // } else if (!password) {
-    //   this.setState({
-    //     pwLengthCondition: true,
-    //     passwordhasValue: false,
-    //   });
-    // }
-    // if (serviceagreement) {
-    //   this.setState({
-    //     isTrueserviceagreement: true,
-    //   });
-    // } else if (!serviceagreement) {
-    //   this.setState({
-    //     isTrueserviceagreement: false,
-    //   });
-    // }
   };
 
   // 버튼 클릭시 실행되는 메소드
@@ -172,92 +113,93 @@ class Account extends React.Component {
       isTrueserviceagreement,
     } = this.state;
 
-    const allAcceptedAccountCondition =
-      firstnamehasValue &&
-      lastnamehasValue &&
-      emailCondition &&
-      pwLengthCondition &&
-      checkpwCondition &&
-      isTrueserviceagreement;
+    // const allAcceptedAccountCondition =
+    //   firstnamehasValue &&
+    //   lastnamehasValue &&
+    //   emailCondition &&
+    //   pwLengthCondition &&
+    //   checkpwCondition &&
+    //   isTrueserviceagreement;
 
-    if (!firstname) {
-      this.setState({
-        firstnamehasValue: false,
-      });
-    } else if (firstname) {
-      this.setState({
-        firstnamehasValue: true,
-      });
-    }
+    // if (!firstname) {
+    //   this.setState({
+    //     firstnamehasValue: false,
+    //   });
+    // } else if (firstname) {
+    //   this.setState({
+    //     firstnamehasValue: true,
+    //   });
+    // }
 
-    if (!lastname) {
-      this.setState({
-        lastnamehasValue: false,
-      });
-    } else if (lastname) {
-      this.setState({
-        lastnamehasValue: true,
-      });
-    }
+    // if (!lastname) {
+    //   this.setState({
+    //     lastnamehasValue: false,
+    //   });
+    // } else if (lastname) {
+    //   this.setState({
+    //     lastnamehasValue: true,
+    //   });
+    // }
 
-    if (!email) {
-      this.setState({
-        emailhasValue: false,
-      });
-    } else if (email) {
-      this.setState({
-        emailhasValue: true,
-      });
-    }
+    // if (!email) {
+    //   this.setState({
+    //     emailhasValue: false,
+    //   });
+    // } else if (email) {
+    //   this.setState({
+    //     emailhasValue: true,
+    //   });
+    // }
 
-    if (!password) {
-      this.setState({
-        passwordhasValue: false,
-      });
-    } else if (password) {
-      this.setState({
-        passwordhasValue: true,
-      });
-    }
+    // if (!password) {
+    //   this.setState({
+    //     passwordhasValue: false,
+    //   });
+    // } else if (password) {
+    //   this.setState({
+    //     passwordhasValue: true,
+    //   });
+    // }
 
-    if (!checkPw) {
-      this.setState({
-        checkPwhasValue: false,
-      });
-    } else if (checkPw) {
-      this.setState({
-        checkPwhasValue: true,
-      });
-    }
+    // if (!checkPw) {
+    //   this.setState({
+    //     checkPwhasValue: false,
+    //   });
+    // } else if (checkPw) {
+    //   this.setState({
+    //     checkPwhasValue: true,
+    //   });
+    // }
 
-    if (!serviceagreement) {
-      this.setState({
-        isTrueserviceagreement: false,
-      });
-    } else if (serviceagreement) {
-      this.setState({
-        isTrueserviceagreement: true,
-      });
-    }
+    // if (!serviceagreement) {
+    //   this.setState({
+    //     isTrueserviceagreement: false,
+    //   });
+    // } else if (serviceagreement) {
+    //   this.setState({
+    //     isTrueserviceagreement: true,
+    //   });
+    // }
 
-    // 모든 유효성 검사 체크
-    if (!allAcceptedAccountCondition) {
-      return;
-    }
+    // // 모든 유효성 검사 체크
+    // if (!allAcceptedAccountCondition) {
+    //   return;
+    // }
 
     fetch(SIGNUP_API, {
       method: "POST",
       body: JSON.stringify({
-        //firstname: firstname,
-        //lastname: lastname,
+        firstname: firstname,
+        lastname: lastname,
         email: email,
         password: password, // 로그인은 pw로 함.
-        //adagreement: adagreement
+        checkpassword: checkPw,
+        adagreement: adagreement,
       }),
     })
-      .then((responce) => responce.json())
+      .then((response) => response.json())
       .then((result) => {
-        console.log({ result });
+        alert(result.message);
 
         //
         if (result.message === "SUCCESS") {
@@ -266,7 +208,51 @@ class Account extends React.Component {
           return;
         }
 
-        alert("실패");
+        if (result.message === "이름을 입력해주세요") {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
+
+        if (result.message === "성을 입력해주세요") {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
+
+        if (result.message === "이메일를 입력해주세요") {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
+
+        if (result.message === "INVALID_EMAIL") {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
+
+        if (result.message === "비밀번호를 입력해주세요") {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
+
+        if (
+          result.message === "비밀번호는 최소 10자, 최대 1000자여야 합니다."
+        ) {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
+
+        if (
+          result.message === "비밀번호 및 확인 비밀번호가 일치하지 않습니다"
+        ) {
+          //localStorage.setItem("token", result.Authorization);
+          this.props.history.push("/"); //2주차에 마이페이지
+          return;
+        }
       });
   };
 
@@ -290,7 +276,30 @@ class Account extends React.Component {
       serviceagreementhasValue,
     } = this.state;
 
-    console.log({ adagreement });
+    const emailcheck = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i.test(
+      email,
+    );
+    const passwordcheck = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/i.test(
+      password,
+    );
+    console.log({
+      firstname,
+      firstnamehasValue,
+      lastname,
+      lastnamehasValue,
+      email,
+      emailCondition,
+      emailhasValue,
+      password,
+      pwLengthCondition,
+      passwordhasValue,
+      checkPw,
+      checkpwCondition,
+      checkPwhasValue,
+      adagreement,
+      serviceagreement,
+      serviceagreementhasValue,
+    });
     return (
       <div className="account">
         <main>
@@ -299,13 +308,14 @@ class Account extends React.Component {
             <article className="inputBundle">
               <div className="firstname accountTextInput">
                 <p>이름</p>
+
                 <input
                   type="text"
                   name="firstname"
                   hasvalue="firstnamehasValue"
                   onChange={this.firstnameInput}
                 />
-                {!firstnamehasValue && (
+                {firstnamehasValue === false && (
                   <p className="warningMsg">이름은 필수 입력 항목입니다.</p>
                 )}
               </div>
@@ -317,28 +327,40 @@ class Account extends React.Component {
                   hasvalue="lastnamehasValue"
                   onChange={this.lastnameInput}
                 />
-                {!lastnamehasValue && (
+                {lastnamehasValue === false && (
                   <p className="warningMsg">성은 필수 입력 항목입니다.</p>
                 )}
               </div>
               <div className="email accountTextInput">
                 <p>이메일</p>
-                <input type="text" name="email" onChange={this.emailInput} />
-                {!emailhasValue && (
+                <div className={`checkemail ${email && "success"}`}>
+                  <input type="text" name="email" onChange={this.emailInput} />
+                  {/* {!emailhasValue && (
                   <p className="warningMsg">이메일은 필수 입력 항목입니다.</p>
                 )}
                 {!emailCondition && (
                   <p className="warningMsg">이메일 형식이 아닙니다.</p>
+                )} */}
+                  <p className={emailcheck ? "true" : "false"}>
+                    {emailcheck ? "" : "이메일 형식이 아닙니다."}
+                  </p>
+                </div>
+                {emailhasValue === false && (
+                  <p className="warningMsg">이메일은 필수 입력 항목입니다.</p>
                 )}
               </div>
               <div className="password accountTextInput">
                 <p>비밀번호</p>
-                <input
-                  type="password"
-                  name="password"
-                  hasvalue="passwordhasValue"
-                  onChange={this.passwordInput}
-                />
+                {/* <div className={`checkpassword ${password && "success"}`}>
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={this.passwordInput}
+                  />
+                  <p className={passwordcheck ? "true" : "false"}>
+                    {passwordcheck ? "" : "숫자/소문자/대문자/특수문자를 포함한 8글자 이상을 작성해주세요."}
+                  </p>
+                </div> */}
                 {!passwordhasValue && (
                   <p className="warningMsg">비밀번호은 필수 입력 항목입니다.</p>
                 )}
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER = "http://192.168.200.163:8000";
+export const SERVER = "http://10.58.6.3:8000";
 export const SIGNUP_API = `${SERVER}/user/signup`;
