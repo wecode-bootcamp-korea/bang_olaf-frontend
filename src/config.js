b72e719f50b84f47c5831356147d84d1564161a9
Jaeyoon <sbjeong222@gmail.com>
2021-01-22 11:35:14 +0900

    Add: 데이터 통신 완료/카테고리 클릭 시 다른 데이터 받아 오기 완료/config.js 충돌 해결

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 export const SERVER = "http://10.58.0.59:8000";
-<<<<<<< HEAD
 export const PRODUCTLIST = `${SERVER}/product/subcategory/list/1`;
-=======
 export const PRODUCTDETAIL_API = `${SERVER}/product/1`;
 export const SIGNUP_API = `${SERVER}/user/signup`;
 export const SIGNIN_API = `${SERVER}/user/signin`;
->>>>>>> main
